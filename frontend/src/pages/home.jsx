f4349825de7279d91419dc48e2338f53df5a8d52
Mrunal-Malkar <mrunalpmalkar@gmail.com>
2025-02-25 19:37:28 +0530

    added connected person div

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,19 +1,41 @@
 import React from 'react'
 import Navbar from '../components/navbar'
+import "./../stylesheets/home.css"
 
 const Home = () => {
   return (
     <>
-    <Navbar/>
+      <Navbar />
 
-    <div className="w-full h-[54rem] flex justify-center items-center bg-[rgb(13, 31, 45)] p-2 md:p-0">
-        <div className="md:w-3/4 md:h-11/12 w-full h-full flex justify-start bg-base-300">
+      <div className="w-full h-[92vh] flex bg-[#288C9B] justify-center items-center">
+        <div className='h-full md:h-11/12 2xl:w-10/12 w-full bg-teal-500 flex'>
+          {/* the start of inner divs */}
+          <div className="w-full min-w-[23rem] md:w-2/6 bg-[#25283D] flex flex-col overflow-auto scrollbardiv">
+
+            {/* connect person div */}
+            <div className='min-h-[80px] bg-gray-700 m-2 flex justify-start items-center p-1'>
             
-            <div className="w-2/6 bg-green-400">s</div>
-            <div className="w-4/6 bg-green-200">ss</div>
+              <div className='min-w-[70px] bg-blue-500 min-h-[70px] max-w-[70px] max-h-[70px] border-2 connectprofilepicdiv'></div> {/* profile pic div */}
+             
+              <div className='flex flex-col p-2 gap-y-1 w-full h-full'> {/* name and message div */}
+                <div className='flex justify-between items-center w-full h-1/2'>
+                  <h1 className='text-xl md:text-2xl'>Mrunal Malkar</h1>
+                  <p>5:00pm</p>
+                </div>
+                <div>
+                  <p>Recent Message</p>
+                </div>
+              </div>
+           
+            </div>
+            {/* end of connect person div */}
 
+          </div>
+          {/* end of first inner div */}
+          <div className='w-4/6 bg-[#2D3047] hidden md:block'>s</div>
+          {/* the end of inner divs */}
         </div>
-    </div>
+      </div>
 
     </>
   )
